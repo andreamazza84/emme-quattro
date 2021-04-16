b8278a53e9db529dd90df5a695c087dc7c3a0b95
andreamazza84 <andrea.mazza84@gmail.com>
2021-04-16 15:52:19 +0200

    sistemazione cartelle

--- src/App.vue
+++ src/App.vue
@@ -29,6 +29,35 @@ export default {
 }
 </script>
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+
+/*Variables*/
+
+/*Colors*/
+
+$darkcolor: #222;
+$lightcolor: #f7f7f7;
+
+/*Font sizes*/
+$xxlarge: 45px;
+$xlarge: 36px;
+$large: 24px;
+$normal: 16px;
+$text: 14px;
+$small: 11px;
+
+/*/Variables*/
+
+.container{
+  width: 80%;
+  height: 100%;
+  max-width: 1170px;
+  margin: auto;
+}
+section{
+  padding: 40px 0;
+}
 
 #app {
   font-family: 'Poppins', sans-serif;
