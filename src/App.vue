9b42d82a2a1e02028a9902b635b670e900ad2157
andreamazza84 <andrea.mazza84@gmail.com>
2021-05-03 11:16:37 +0200

    lavoro su navbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <div id="app">
     <div id="nav">
+    <section class="top">
       <div class="container">
-        <router-link to="/">Home</router-link>
         <router-link to="/chi-siamo">Chi Siamo</router-link>
         <router-link to="/prodotti">Prodotti</router-link>
         <router-link to="/servizi">Servizi</router-link>
+        <router-link to="/" id="home"><div class="logo"></div></router-link>
         <router-link to="/news">News</router-link>
         <router-link to="/contatti">Contatti</router-link>
-        <router-link to="/area-clienti">Area Clienti</router-link>
+        <router-link to="/area-clienti">Area<br>Clienti</router-link>
       </div>
+    </section>
+    <section class="bottom">
+      <div class="container">
+        <div class="jingle">il colore su misura</div>
+      </div>
+    </section>
     </div>
     
     <router-view id="main"/>
@@ -21,8 +28,6 @@
 </template>
 
 <script>
-  import axios from 'axios'
-  import VueAxios from 'vue-axios'
   export default {
   data(){
     return {}
@@ -46,8 +51,5 @@
 </script>
 
 <style lang="scss">
-  @import "~@/scss/commons/variables.scss";
-  @import "~@/scss/commons/common.scss";
-  @import "~@/scss/commons/general.scss";
-  @import "~@/scss/commons/12frame.scss";
+  @import "~@/scss/app.scss";
 </style>
