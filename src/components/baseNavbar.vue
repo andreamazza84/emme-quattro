febf4b1eafe846bb83bffe893aff1d3e17cbbb7d
andreamazza84 <andrea.mazza84@gmail.com>
2021-05-03 20:08:36 +0200

    sistemare overlay

--- src/components/baseNavbar.vue
+++ src/components/baseNavbar.vue
@@ -1,6 +1,19 @@
 <template>
   <section id="navbar">
-    <div class="container">
+    <div v-if="showBars" class="container">
+      <router-link to="/" class="home-page"><div class="logo"></div></router-link>
+      <i class="menu-hamburger" @click="mediaMenu()"></i>
+      <div v-if="showMenu" class="overlay">
+        <router-link to="/">Home</router-link>
+        <router-link to="/chi-siamo" >Chi Siamo</router-link>
+        <router-link to="/prodotti" >Prodotti</router-link>
+        <router-link to="/servizi" >Servizi</router-link>
+        <router-link to="/news">News</router-link>
+        <router-link to="/contatti">Contatti</router-link>
+        <router-link to="/area-clienti">Area<br>Clienti</router-link>
+      </div>
+    </div>
+    <div v-else class="container">
       <router-link to="/chi-siamo" >Chi Siamo</router-link>
       <router-link to="/prodotti" >Prodotti</router-link>
       <router-link to="/servizi" >Servizi</router-link>
@@ -8,7 +21,6 @@
       <router-link to="/news">News</router-link>
       <router-link to="/contatti">Contatti</router-link>
       <router-link to="/area-clienti">Area<br>Clienti</router-link>
-      <div class="menu-hamburger"></div>
     </div>
   </section>
 </template>
@@ -16,6 +28,8 @@
 export default {
   data(){
     return{
+      showBars: false,
+      showMenu: false,
       window: {       // dichiarazione iniziale per la variabile window
         width: 0,
         height: 0,
@@ -24,35 +38,50 @@ export default {
   },
   methods:{
     /**  
-    * Cambia il numero di cards da mostrare in base alla larghezza della finestra.   
+    * @window_resize Shows menu hamburger under 992px screen width    
     */
-    mediaQuery: function() {
+    mediaWidth: function() {
         this.window.width = window.innerWidth;
         this.window.height = window.innerHeight;
         let width = 0;
         //tablet
-        if(window.innerWidth <= 992 && window.innerWidth > 768) {
-            this.show = this.size.md; //2
+        if(window.innerWidth > 992) {
+          this.showBars = false;
+          this.showManu = false;
         }
-        //mobile
-        else if(window.innerWidth  <= 768){
-            this.show = this.size.sm; //1
-        }
+        // //mobile
+        // else if(window.innerWidth  <= 768){
+        //     this.show = this.size.sm; //1
+        // }
         //desktop
-        else if(window.innerWidth  > 992){
-            this.show = this.size.lg; //3
+        if(window.innerWidth  <= 992){
+            this.showBars = true;
         }
-        width = (100/this.show);
-        this.cardWidth = width.toString() + '%';
+        //width = (100/this.show);
+        //this.cardWidth = width.toString() + '%';
         //console.log(this.cardWidth);
-        return this.cardWidth;
+    },
+    /**
+      @click shows the menu for mobile devices
+     */
+    mediaMenu: function(){
+      if(this.showMenu){
+        this.showMenu = false;
+      }
+      else{
+        this.showMenu = true;
+      }
+      //console.log(this.showMenu);
     },
   },
   created() {
-    window.addEventListener('resize', this.mediaQuery);
+    window.addEventListener('resize', this.mediaWidth);
+  },
+  mounted(){
+    this.mediaWidth();
   },
   distroyed() {
-    window.removeEventListener('resize', this.mediaQuery);
+    window.removeEventListener('resize', this.mediaWidth);
   },
 }
 </script>
