a3976c0849cdc30c9c499b5382693a893e764969
andreamazza84 <andrea.mazza84@gmail.com>
2021-05-11 15:34:54 +0200

    overlay ok

--- src/views/Prodotti.vue
+++ src/views/Prodotti.vue
@@ -1,33 +1,32 @@
 <template>
-    <div class="prodotti_servizi">
-        <section class="prodotti">
-            
-            <div class="container">
-              <div class="card-container row">
-                <div class="card col-lg-4 col-md-4 col-sm-12" 
-                v-for="prodotto in prodotti" 
-                :key="prodotto.slug"
-                >
-                  <router-link class="innerlink" 
-                    :to="{
-                      name: 'ProdottiDescrizione',
-                      params: {slug: prodotto.slug}
-                    }"
-                  >
-                      <div class="img" :style="`background-image: url(${prodotto.acf.image.url})`" @click="scrollToTop()">
-                        <h4 class="title" v-html="prodotto.title.rendered"></h4>
-                      </div>
-                      <!-- <img :src="prodotto.acf.image.url" :alt="prodotto.acf.image.alt"> -->
-                  </router-link>
-                </div>
-              </div>
-            </div>
-                
-            <!-- Router View  -->
-            <router-view :key="$route.path" />
-            <!-- /Router View  -->
+  <div class="prodotti_servizi">
+    <section class="prodotti">
+        
+      <div class="container">
+        <div class="card-container row">
+          <div class="card col-lg-4 col-md-6 col-sm-12" 
+          v-for="prodotto in prodotti" 
+          :key="prodotto.slug"
+          >
+            <router-link class="innerlink" 
+              :to="{
+                name: 'ProdottiDescrizione',
+                params: {slug: prodotto.slug}
+              }"
+            >
+                <h4 class="title" v-html="prodotto.title.rendered"></h4>
+                <div class="img" :style="`background-image: url(${prodotto.acf.image.url})`"></div>
+                <!-- <img :src="prodotto.acf.image.url" :alt="prodotto.acf.image.alt"> -->
+            </router-link>
+          </div>
+        </div>
+      </div>
+          
+      <!-- Router View  -->
+      <router-view :key="$route.path" />
+      <!-- /Router View  -->
 
-        </section>
+    </section>
     </div> 
 </template>
 <script>
@@ -39,7 +38,7 @@ export default {
   },
   methods:{
     scrollToTop: function(){
-      window.scrollTo({top: 20, behavior: 'smooth'});
+      window.scrollTo({top: 0, behavior: 'smooth'});
     },
   },
 }
--- src/views/ProdottiDescrizione.vue
+++ src/views/ProdottiDescrizione.vue
@@ -1,23 +1,35 @@
 <template>
   <section id="descrizione-prodotti">
-    <div class="container">
-      <div class="box row">
-        <div class="col-lg-4">
+    <div class="box-container container">
+      <div class="box-row row">
+        <div class="col-lg-4 col-md-6 col-sm-12">
           <div class="img" :style="`background-image: url(${prodotto.acf.image.url})`" :alt="prodotto.acf.image.alt">
           </div>
           <!-- <img :src="prodotto.acf.image.url"> -->
         </div>
-        <div class="col-lg-6">
+        <div class="col-lg-7 col-md-5 col-sm-10 col-offset-sm-1">
           <h3 class="title product-title"><span v-html="prodotto.title.rendered"></span></h3>
           <p class="descrizione"><span v-html="prodotto.content.rendered"></span></p>
         </div>
-        <div class="col-lg-2">
-          <div class="download">
-            <div class="pdf"></div>
+      </div>
+      
+    <router-link to="/prodotti" class="close-link"><div class="close"></div></router-link>
+    
+    </div>
+
+    <div class="box-container container" v-if="prodotto.acf.file">
+      <div class="box-row row">
+        <div class="col-lg-6">
+          <div class="download acqua">
+            <a class="pdf" :href="prodotto.acf.file.url">{{prodotto.title.rendered}} <span>all'acqua</span></a>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="download solvente">
+            <a class="pdf" :href="prodotto.acf.file.url">{{prodotto.title.rendered}} <span>al solvente</span></a>
           </div>
         </div>
       </div>
-    <router-link to="/prodotti" class="close-link"><div class="close" @click="scrollToTop()"></div></router-link>
     </div>
     
   </section>
