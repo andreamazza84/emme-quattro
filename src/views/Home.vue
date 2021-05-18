873b1dbd6e9dbfd895e4c9a341121d402a3b1ba1
andreamazza84 <andrea.mazza84@gmail.com>
2021-05-18 18:09:24 +0200

    home: trovare immagini
    aggiungere titoli articoli - news

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="home pt-90">
   <section class="container top">
-    <carousel class="carousel" 
+    <carousel class="carousel big-carousel" 
     :per-page="1"
     :autoplay="true"
     :autoplayTimeout="3000"
@@ -26,9 +26,7 @@
         <div class="search-for">
           Cerchi una vernice oppure un colore in particolare?<br>Con M4 hai tante soluzioni per i <span>privati</span> e tanti servizi dedicati alle <span>imprese</span>
         </div>
-        <router-link class="btn" to="/contatti">
-          Contattaci
-        </router-link>
+        <router-link class="btn" to="/contatti"> <span @click="scrollToTopAuto()">Contattaci</span></router-link>
       </div>
     </div>
   </section>
@@ -36,24 +34,47 @@
 
   <section class="container90 bottom">
     <div class="row area-riservata">
-      <div class="col-lg-6 left">
-        <div class="img" :style="`background-image: url(${areaClientiImg});`"></div>
-        <!-- <img src="~/@/assets/dima-pechurin-JUbjYFvCv00-unsplash_1200x800.jpg" alt="area clienti"> -->
+      <div class="col-lg-6 col-md-6 col-sm-12 left">
+        <!-- <img src="~@/assets/dima-pechurin-JUbjYFvCv00-unsplash_1200x800.jpg" alt="area clienti"> -->
       </div>
-      <div class="col-lg-6 right">
+      <div class="col-lg-6 col-sm-12 right">
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor officiis, odio consequatur ad ullam quod recusandae repellat facere ex quis asperiores molestiae ipsum hic eum aspernatur, cum alias sint voluptas.</p>
-        <router-link class="btn" to="/area-clienti">Accedi all'area riservata</router-link>
+        <router-link class="btn" to="/area-clienti"><span @click="scrollToTopAuto()">Accedi all'area riservata</span></router-link>
       </div>
-
     </div>
     
-    <div class="row">
-      <div class="col-lg-12"></div>
+    <div class="row contattaci">
+      <div class="col-lg-6 col-md-8 col-sm-12 call-out-col">
+        <div class="call-out shadow">
+          <div class="search-for">
+            Contattaci per maggiori informazioni
+          </div>
+          <router-link class="btn" to="/contatti">
+            <span @click="scrollToTopAuto()">Contattaci</span>
+          </router-link>
+        </div>
+      </div>
+      <div class="col-lg-6 col-md-4 col-sm-0"></div>
     </div>
     
-    <div class="row">
-      <carousel>
-      
+    <div class="row articles">
+      <carousel class="carousel small-carousel"
+      :per-page="show"
+      :autoplay="true"
+      :autoplayTimeout="3000"
+      :loop="true"
+      :paginationEnabled="true"
+      paginationPosition="bottom-overlay"
+      paginationActiveColor="#ed7124"
+      paginationColor="#ed712455"
+      :paginationSize="15"
+      :paginationPadding="15"
+      :touchDrag="true"
+      :mouseDrag="true"
+      >
+        <slide class="slide" v-for="image in carosello" :key="image.slug">
+          <div class="image" :style="`background-image:url(${image.acf.image.url})`"></div>
+        </slide>
       </carousel>
     </div>
   </section>
@@ -74,13 +95,51 @@ export default {
   },
   data(){
     return {
-      areaClientiImg: "~/@/assets/dima-pechurin-JUbjYFvCv00-unsplash_1200x800.jpg",
+      window:{
+        width: 0
+      },
+      show: 4,
     }
   },
   computed:{
     carosello: function(){
       return this.$store.state.carosello
     }
+  },
+  methods:{
+    scrollToTopAuto: function(){
+      window.scrollTo({top: 0, behavior: 'auto'});
+    },
+    /** Shows menu hamburger under 992px screen width
+    * @window_resize     
+    */
+    mediaWidth: function() {
+        //mobile 768px
+        //tablet 992px
+        //desktop 1200px
+        this.window.width = window.innerWidth;
+        if(this.window.width  <= 992 && this.window.width  > 768){
+          this.show = 2;
+        }
+        else if(this.window.width  <= 1200 && this.window.width  > 992){
+          this.show = 3;
+        }
+        else if(this.window.width  <= 768){
+          this.show = 1;
+        }
+        else if(this.window.width > 1200) {
+          this.show = 4;
+        }
+    },
+  },
+   created() {
+    window.addEventListener('resize', this.mediaWidth);
+  },
+  mounted(){
+    this.mediaWidth();
+  },
+  distroyed() {
+    window.removeEventListener('resize', this.mediaWidth);
   },
 }
 </script>
