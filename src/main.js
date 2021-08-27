45b9a04d7b50f0a922b9d0c923d5cdd95f76d95e
andreamazza84 <andrea.mazza84@gmail.com>
2021-08-27 20:26:04 +0200

    API key google problem

--- src/main.js
+++ src/main.js
@@ -1,32 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
-
 // Vue-Router
 import router from './router'
-
 // Vuex
 import store from '@/store'
-
 // Axios
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-
-// Font Awesome
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
 // Google Maps
 import * as VueGoogleMaps from 'vue2-google-maps'
-
+// Vuetify
 import vuetify from './plugins/vuetify'
-
-/* ---------------------------------- */
 
 // Google Maps
 Vue.use(VueGoogleMaps, {
   load: {
-    key: '',
+    key: 'AIzaSyD2OJw8sN-KSp5G9d4e_5kswxA8hAVmxSM',
     libraries: 'places',
   }
 });
@@ -36,13 +25,8 @@ Vue.use(VueAxios, axios);
 
 // *!* Cambiare in fase di deploy 
 //Vue.axios.defaults.baseURL = 'http://localhost:8880/wp-json';
-//Vue.axios.defaults.baseURL = 'https://emmequattro.altervista.org/wordpress/wp-json';
-Vue.axios.defaults.baseURL = 'https://www.m4srl.it/wordpress/wp-json';
-
-
-//FontAwesome
-//library.add(faUserSecret)
-//Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.axios.defaults.baseURL = 'https://emmequattro.altervista.org/wordpress/wp-json';
+// Vue.axios.defaults.baseURL = 'https://www.m4srl.it/wordpress/wp-json';
 
 Vue.config.productionTip = false
 
