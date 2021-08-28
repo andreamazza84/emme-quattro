9ecb997120ee4e0ccf6469b9701f5c180662fb11
andreamazza84 <andrea.mazza84@gmail.com>
2021-08-28 09:58:43 +0200

    git pages

--- vue.config.js
+++ vue.config.js
@@ -9,14 +9,16 @@ module.exports = {
       // `scss` syntax requires an semicolon at the end of a statement, while `sass` syntax requires none
       // in that case, we can target the `scss` syntax separately using the `scss` option
       scss: {
-        additionalData: `@import "~@/scss/commons/variables.scss";
-                      @import "~@/scss/commons/common.scss";
-                      @import "~@/scss/commons/general.scss";`
+        additionalData: `@import "~@/scss/commons/variables.scss"; @import "~@/scss/commons/common.scss"; @import "~@/scss/commons/general.scss";`
       },
     }
   },
 
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+
+  outputDir: 'docs',
+
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/docs'
 }
